15eb993dd0b1371f95f8054222c22c8721c7dc10
andrew <dave.solatre@yahoo.com.ph>
2023-03-13 11:20:56 +0800

    first commit

---
+++ src/code.java
@@ -0,0 +1,20 @@
+import org.testng.annotations.Test;
+
+public class code {
+	
+	
+@Test
+public void gitHello() {
+	System.out.println("hello");
+}
+
+@Test
+public void gitBye() {
+	System.out.println("BYers");
+}
+	
+
+	public static void main(String[] srgs) {
+
+	}
+}
